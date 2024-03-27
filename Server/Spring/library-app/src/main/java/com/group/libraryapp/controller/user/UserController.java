3b9474bd3a19956b4f5cfed294e81c00886a831b
Nyeong <iwbm312@gmail.com>
2024-03-27 11:27:32 +0900

    [Spring] db연결;user 생성, 조회 API 수정;수정, 삭제 API 개발

--- Server/Spring/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ Server/Spring/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,34 +2,63 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
+    // jdbc == java database connector
+    private final JdbcTemplate jdbcTemplate;
 
-    private final List<User> users = new ArrayList<>();
+    public UserController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user") // POST /user
     public void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getName(), request.getAge()));
-        // json형식으로 name, age가 들어오면 UserCreateRequest 객체에 값이 매핑되고, 그 값들은 새로운 User객체를 생성하는 데에 사용된다. 새로운 User가 정상적으로 생성되고 나서 User 리스트에 저장되면, 200 OK를 반환한다.
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
     @GetMapping("/user")
     public List<UserResponse> getAllUsers(){
-        List<UserResponse> responses = new ArrayList<>();
-        for (int i = 0; i < users.size(); i++) {
-            responses.add(new UserResponse(i + 1, users.get(i)));
-        }
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
 
-        return responses;
+//        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
+//            // sql query를 통해 가져온 user정보를 UserResponse객체로 바꿔주는게 mapRow의 기능
+//            @Override
+//            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                long id = rs.getLong("id"); // 결과에서 id라는 이름의 long type 변수를 가져옴
+//                String name = rs.getString("name");
+//                int age = rs.getInt("age");
+//                return new UserResponse(id, name, age);
+//            }
+//        });
     }
 
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request){
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name) { // 바디를 쓰지 않고 쿼리를 사용함. 객체를 사용할 수도 있지만, 변수가 1개이기 때문에 간단하게 쿼리파라미터로 받음.
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
+    }
 }

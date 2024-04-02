90f84a610b2cdc87e31df0d8fbec0053949bb35a
Nyeong <iwbm312@gmail.com>
2024-04-02 18:32:04 +0900

    [Spring] notification-server initial commit

---
+++ Server/Spring/notification-server-prac/src/main/java/com/jinyshin/notificationserverprac/NotificationServerPracApplication.java
@@ -0,0 +1,13 @@
+package com.jinyshin.notificationserverprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServerPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServerPracApplication.class, args);
+	}
+
+}
---
+++ Server/Spring/notification-server-prac/src/test/java/com/jinyshin/notificationserverprac/NotificationServerPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinyshin.notificationserverprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServerPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

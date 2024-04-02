2fd4bd63a6ab9178da251841a7708229a1f3d898
Nyeong <iwbm312@gmail.com>
2024-04-02 18:31:39 +0900

    [Spring] user-service initial commit

---
+++ Server/Spring/user-service-prac/src/main/java/com/jiny/userserviceprac/UserServicePracApplication.java
@@ -0,0 +1,13 @@
+package com.jiny.userserviceprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServicePracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServicePracApplication.class, args);
+	}
+
+}
---
+++ Server/Spring/user-service-prac/src/test/java/com/jiny/userserviceprac/UserServicePracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiny.userserviceprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServicePracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

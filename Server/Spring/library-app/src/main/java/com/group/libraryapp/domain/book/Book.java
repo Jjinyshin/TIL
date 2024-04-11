7f19a830a6a4a83c882e21a273daf34d49ac7c44
Jinyshin <iwbm312@gmail.com>
2024-04-11 14:02:23 +0900

    [Spring] 책 생성 API 개발
    - Table 생성
    - Domain Entity 생성
    - Repository 생성
    - DTO 생성
    - Controller 생성
    - Service 생성

---
+++ Server/Spring/library-app/src/main/java/com/group/libraryapp/domain/book/Book.java
@@ -0,0 +1,26 @@
+package com.group.libraryapp.domain.book;
+
+import javax.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+    @Column(nullable = false) // @Column의 length 기본값은 255
+    private String name;
+
+    public Book(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(String.format("잘못된 name(%s)이 입력되었습니다.", name));
+        }
+        
+        this.name = name;
+    }
+
+    protected Book() {
+
+    }
+}
---
+++ Server/Spring/library-app/src/main/java/com/group/libraryapp/domain/book/BookRepository.java
@@ -0,0 +1,8 @@
+package com.group.libraryapp.domain.book;
+
+import com.group.libraryapp.domain.book.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    void saveBook(String name);
+}

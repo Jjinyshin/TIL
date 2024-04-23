39a48b9e91365e0873fdf95e402344df51896ea6
Jinyshin <iwbm312@gmail.com>
2024-04-23 11:34:00 +0900

    [Algorithm] 프로그래머스 시저 암호 - Level 1

---
+++ Algorithm/programmers/caesar cipher/src/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                c = (char) ((c - 'A' + n) % 26 + 'A');
+            } else if (c >= 'a' && c <= 'z') {
+                c = (char) ((c - 'a' + n) % 26 + 'a');
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}

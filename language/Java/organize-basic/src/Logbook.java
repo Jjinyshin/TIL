a9cf4ceb133968cfba4950e72dcd923a1dc7da65
Jinyshin <iwbm312@gmail.com>
2024-05-14 21:49:56 +0900

    [Java] refactoreded code

--- language/Java/organize-basic/src/Logbook.java
+++ language/Java/organize-basic/src/Logbook.java
@@ -8,11 +8,13 @@ import java.util.Collections;
 
 public class Logbook {
     void wirteMessage(String message, Path location) throws IOException {
-        if (Files.isDirectory(location)) {
-            throw new IllegalArgumentException("The path is invalid");
-        }
-        if (message.trim().equals("") || message == null) {
+//        인수를 검증할 때는 순서가 중요한데, 반드시 null을 먼저 확인한 후 도메인에 따라 "유효하지 않은" 값을 검사해야 한다.
+//        메서드 서명 내 인수 순서에 따라 확인하도록 순서 변경
+        if (message == null || message.trim().isEmpty()) {
             throw new IllegalArgumentException("The message is invalid");
+        }
+        if (location == null || Files.isDirectory(location)) {
+            throw new IllegalArgumentException("The path is invalid");
         }
         String entry = LocalDate.now() + ": " + message;
         Files.write(location, Collections.singletonList(entry),
